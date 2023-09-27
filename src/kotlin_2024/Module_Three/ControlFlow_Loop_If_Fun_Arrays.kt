09bb35c6e1869b92588c826ac69daecc488872dc
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-09-27 07:45:50 +0500

    If Else

---
+++ src/kotlin_2024/Module_Three/ControlFlow_Loop_If_Fun_Arrays.kt
@@ -0,0 +1,25 @@
+package kotlin_2024.Module_Three
+
+//class ControlFlow_Loop_If_Fun_Arrays {
+//}
+
+fun main() {
+    // if Else
+    var animal = "Dog"
+
+    if (animal == "Cat"){
+
+    }
+    else if (animal == "Dog"){
+        println("The animal is a Dog")
+    }
+
+    // When Kotlin
+    when(animal){
+        "Cat"-> println("The animal is a Car")
+        "Dog"-> println("The animal is a Dog")
+    }
+
+
+
+}
