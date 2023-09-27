37dfd3db19f52193db07861ac8100b46606e45a9
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-09-27 08:19:32 +0500

    Loops for do while

---
+++ src/kotlin_2024/Module_Three/Loops.kt
@@ -0,0 +1,46 @@
+package kotlin_2024.Module_Three
+
+fun main() {
+    // Loops
+
+    //While loop
+    var i = 1
+    //var sum = 0
+    while (i <= 6){
+        println("Line $i ")
+        i++
+        //sum += i // sum = sum + i
+        //--i
+    }
+    //println("Sum = $sum")
+
+    // do While loop
+//    var sum = 0
+//    var input:String
+    var index = 7
+    println()
+   /* do {
+        // perform a set of action while the value of index is  not equal to 0
+        print("The val of index is $index")
+//        print("Enter an integer")
+//        input = readLine()!!
+//        sum += input.toInt()
+    } while (index != 0)*/
+//    println("Sum $sum")
+
+    do {
+        // perform a set of action while the value of index is  not equal to 0
+        println("The val of index is $index")
+        --index
+    } while (index != 0)
+
+    println()
+
+    // For Loop
+    var name = "Scaler"
+    for (eachText in name){
+        println("Sum is: $eachText")
+    }
+
+    for ()
+}
