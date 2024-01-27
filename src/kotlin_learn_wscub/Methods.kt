dc021e64f20f0f1bbcc0f4ac12407eadf0e2d720
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-01-27 10:18:33 +0500

    Practice 01/27/2024

--- src/kotlin_learn_wscub/Methods.kt
+++ src/kotlin_learn_wscub/Methods.kt
@@ -2,26 +2,28 @@ package kotlin_learn_wscub
 
 class Methods {
 
+    // Kotlin Methods Practice
     companion object{
+
+        // Main Fun
         @JvmStatic
         fun main(args: Array<String>) {
 
             println("The sum is ${add(5,6, 10)}")
         }
 
+        // Method 1
         fun add(a : Int , b : Int) : Any {
 
             return a+b
         }
 
+        // Method 2
         fun add(a : Int , b : Int, c: Int) : Any {
 
             return a+b+c;
         }
 
-
     }
-
-
 
 }
