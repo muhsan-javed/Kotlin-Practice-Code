4809d381cc53f90c1fdcad94176059ffdde8d407
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-01-28 07:07:41 +0500

    Practice 01/28/2024 Add Notes

--- src/kotlin_learn_wscub/companion_Object_example.kt
+++ src/kotlin_learn_wscub/companion_Object_example.kt
@@ -1,9 +1,18 @@
 package kotlin_learn_wscub
 
-
-
-
 fun main() {
+
+    // ********************  Companion Object *********************************
+    /*
+    In Kotlin or any other programming language like Java and C#.
+
+    whenever we want to call the method or whenever we want to access the members of a class then we make the object of the class and with the help of that object, we access the members of the class.
+
+    In some languages like Java and C#, we use static keyword to declare the members of the class and use them without making any object i.e. just call them with the help of class name.
+
+    There is nothing called static in Kotlin. So, in Kotlin, we use a companion object.
+     */
+
     val co = companion_Object_example()
     println("First No is: "+ co.firstNo)
     println("Sum is: "+ co.Add(6,5))
