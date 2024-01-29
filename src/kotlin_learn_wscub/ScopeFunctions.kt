65d608278b68d079299c32a2a892faef03e1a090
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-01-29 13:33:29 +0500

    Practice 01/29/2024 Add Notes

--- src/kotlin_learn_wscub/ScopeFunctions.kt
+++ src/kotlin_learn_wscub/ScopeFunctions.kt
@@ -2,54 +2,72 @@ package kotlin_learn_wscub
 
 
 fun main() {
+    /*             ********************  Scope Functions Kotlin *********************************
+    => The Kotlin standard library contains several functions whose sole purpose is to execute a block of code within the context of an object.
+    => When you call such a function on an object with a lambda expression provided, it forms a temporary scope.
+    => In this scope, you can access the object without its name.
 
-    var name: String = "Muhsan"
+     TYPE of SCOPE FUNCTIONS
+        with-> Return: Lambda result | Context object: this
+        let->  Return: Lambda result | Context object: it
+        run->  Return: Lambda result | Context object: this
 
-    name.let {
-        println("The lenght of the name is: $it?.length")
-        println("The name is: "+ it)
-        println("The reversed name is: "+ it?.reversed())
+        apply-> Return: context object | Context object: this
+        also->  Return: context object | Context object: it
+
+       */
+
+    // let
+
+    var name: String? = "Muhsan"
+    name?.let {
+        println("The lenght of the name is: ${it.length}")
+        println("The name is: $it")
+        println("The reversed name is: " + it.reversed())
 
     }
 
-    val user: User?  = null
+    val user = User()
 
-    val age = user?.run{
-        println("The name if user is ${user.name}")
+    // run
+    val run = user?.run {
+        println("The name if user is ${this.name}")
         age
         "Run Scope Function"
     }
-    println("Msg is: $age")
+    println("Msg is: $run")
 
-//    val user = User().apply {
-//        name = "Muhsan"
-//        age = 50
-//        this.mobileNumber = 06874564.4
-//
-//    }
+    // with
+    val ageWith = with(user){
+        println("The name if user is ${user.name}")
+        age
+    }
 
-//    val age = with(user){
-//        println("The name if user is ${user.name}")
-//        age
-//    }
-//    println("The age of user is $age")
+    println("The age of user is $ageWith")
 
-//    user.also {
-//        it.name = "Uzair"
-//        it.age = 100
-//        println("The Name Hsa Changed !")
-//    }
-//
-//    println("The Name is : ${user.name}")
+    // apply
+   /* val user = User().apply {
+        name = "Muhsan"
+        age = 50
+        this.mobileNumber = 06874564.4
+    }*/
+
+    // also
+   /* user.also {
+        it.name = "Uzair"
+        it.age = 100
+        println("The Name Hsa Changed !")
+    }
+
+    println("The Name is : ${user.name}")*/
 
 
 }
 
-class User{
+class User {
     var name: String = "Muhsan javed"
-    var age : Int = 20
+    var age: Int = 20
     var mobileNumber: Double = 00.10
-
 }
 
 class ScopeFunctions {
