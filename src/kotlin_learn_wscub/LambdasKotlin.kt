250114a996e4c9b477e2d9e9dfb45b3141d139e0
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-01-29 13:33:45 +0500

    Practice 01/29/2024 Add Notes

--- src/kotlin_learn_wscub/LambdasKotlin.kt
+++ src/kotlin_learn_wscub/LambdasKotlin.kt
@@ -3,8 +3,21 @@ package kotlin_learn_wscub
 
 fun main() {
 
+    /*
+              ********************  Lambdas Kotlin *********************************
+    => It is just a Function without name.
+    => Also known as Anonymous Functions.
+
+            Parameters { Body }
+                       { x,y -> x + y }
+                       { x -> x * y }
+                       { x,y,z -> x * y + z}
+                       { str -> println(str)}
+
+    */
+
     println("The Square of 2 is: " + sqr(2))
-    println("The Square of 2 is: " + add(2,6))
+    println("The Sum is: " + add(2, 6))
 
     printName()
     printNames("Muhsan Javed")
@@ -14,19 +27,20 @@ fun main() {
 }
 
 //var num :Int = 11
-val sqr : (Int) -> Int = {x -> x*x}
+val sqr: (Int) -> Int = { x -> x * x }
 
-val add : (Int, Int) -> Int = {x,y -> x+y}
+val add: (Int, Int) -> Int = { x, y -> x + y }
 
 val printName: () -> Unit = { println("Hello Android Developer") }
 val printNames: (String) -> Unit = { strName -> println("Hello $strName") }
-val printNameAge = { strName :String -> println("Hello Android $strName") }
+val printNameAge = { strName: String -> println("Hello Android $strName") }
 
 val printAge = { println("19") }
 
 
-fun Square(a:Int): Int{
-    return a*a
+fun Square(a: Int): Int {
+    return a * a
 }
+
 class LambdasKotlin {
 }
