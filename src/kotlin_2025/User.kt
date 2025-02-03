8e4568b047cb2bfedae5a3fdd4472a83f0f385ad
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2025-02-03 09:43:27 +0500

    DataTypes

---
+++ src/kotlin_2025/User.kt
@@ -0,0 +1,36 @@
+package kotlin_2025
+
+fun main() {
+
+    var name ="Shyam"
+    val userId=1613
+    var address = "Qambar Ali khan, PK"
+
+    println()
+    println("User name is ${name}")
+    println("User ID is: $userId")
+    println(address)
+    println("______________")
+
+    var a = 45
+    var b = 89
+    println("Sum of $a and $b is ${a+b}")
+    a = 44
+    b = 56
+    println("Sum of $a and $b is ${a+b}")
+
+    var num:Int = 67
+    var num1 = 14.99
+
+    var fatherName = "Javed Hussain"
+
+
+
+
+
+
+
+
+
+}
+
