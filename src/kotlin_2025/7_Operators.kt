9e750b2d5a7f5f6c833157657c399845dd2c9f84
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2025-02-04 10:21:42 +0500

    Update

---
+++ src/kotlin_2025/7_Operators.kt
@@ -0,0 +1,30 @@
+package kotlin_2025
+
+fun main() {
+    //  OPERATORS KOTLIN
+   /* Operators are the special symbol in programming languages which is used to program special task or special operation.
+   A+B + - / * % a++
+
+     */
+    // Arithmetic Operations
+    val x = 100
+    val y = 50
+
+    println(x+y)
+    println(x-y)
+    println(x/y)
+    println(x*y)
+    println(x%y)
+    var temp = 33
+    temp++
+    val inc = temp.inc()
+    println("Increment: "+temp)
+    println("Increment INC: "+inc)
+    println(x.plus(y))
+    println(x.minus(y))
+
+    // Assignment Operations
+    var name = "Jannat JAN"
+
+    // Comparison Operations
+}
