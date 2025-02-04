0485265f4bb75e7c59ec8b520297024be54c4230
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2025-02-04 10:52:36 +0500

    keywords

---
+++ src/kotlin_2025/8_Keywords.kt
@@ -0,0 +1,33 @@
+package kotlin_2025
+import java.util.ArrayList as SubList
+
+fun main() {
+    // as Keyword
+    val ob: Any = "String"
+    val strob: String = ob as String
+    println(strob.length)
+
+    val list = SubList<String>()
+    list.add("Hello")
+    list.add("World")
+    list.add("António Adamu")
+    list.add("Paula Ho")
+    println(list)
+    println(list::class.java.name)
+
+    // in Keyword
+    for (i in 0..10){
+        println(i)
+    }
+
+    // is KEyword
+
+}
+
+interface MTInter<in T> {
+    fun mtd(value: T)
+}
+
+interface MJInter<out T> {
+    fun mtd(): T
+}
