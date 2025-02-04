ca9d77600429bcade0a74b645d0f248b44e4c1bc
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2025-02-04 10:21:54 +0500

    Update

---
+++ src/kotlin_2025/6_Loops.kt
@@ -0,0 +1,27 @@
+fun main() {
+    // Loops Example
+
+    println("Name is Muhsan")
+    println("Name is Muhsan")
+//    println("Name is Muhsan") 100 time....
+
+    // While loop
+    var count = 1
+   /* while (count <=10){
+        // body
+        println("Jannat")
+        count++
+    }
+    println("By Jnt")*/
+
+    // Do While Loop
+   /* do {
+        println("Jannat ❤️😍  $count")
+        count++
+    }while (count <= 10)*/
+
+    // For Loop
+    for (i in 1..100){
+        println(" ❤ Jannat ❤️😍  $i")
+    }
+}
