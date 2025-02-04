7302d638ff5d04665cc0f9b8a8f43fc7f8860a25
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2025-02-04 10:22:03 +0500

    Update

---
+++ src/kotlin_2025/4_Input_Control.kt
@@ -0,0 +1,12 @@
+package kotlin_2025
+
+fun main() {
+    println("Enter name")
+    val name = readln()
+    println("Hello , $name")
+
+    // Number
+    println("Enter Number")
+    val Number = readln().toInt()
+    println("Number , ${Number+5}")
+}
