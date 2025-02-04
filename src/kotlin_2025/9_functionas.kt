baa8cd68c54adf9d6258c054d498cf16f62b0720
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2025-02-04 12:03:58 +0500

    function practice

--- src/kotlin_2025/9_functionas.kt
+++ src/kotlin_2025/9_functionas.kt
@@ -1,10 +1,32 @@
 package kotlin_2025
 
+// Create a function fun keyword
+fun greet(name: String) {
+    println("Hello, $name")
+}
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
 fun main() {
+    // Create a function fun keyword
 
-    // Single Line Comment
-    println("Hello, World!") // This is a single line comment
+    // Call the function
+    greet("Jannat")
+    //add(78,22)
 
+    // Lambda function
+    val sumLambda: (Int, Int) -> Int = { a, b -> a + b }
+    //println(sumLambda(10, 20))
 
+    // HigherOrderFunction
+    val multiply: (Int, Int) -> Int = { a, b -> a * b }
+    println(higherOrderFunction(5, 5, multiply))
+    // Anonymous function
+    val multiplyAnonymous: (Int, Int) -> Int = { a, b -> a * b }
+    println(higherOrderFunction(6, 6, multiplyAnonymous))
+}
 
+fun higherOrderFunction(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
+    return operation(a, b)
 }
